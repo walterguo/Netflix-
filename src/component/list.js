b0c63c157839dba38b18ebac0d40a39c1737bd80
walterguo <taguo92@gmail.com>
2018-06-06 08:36:15 -0700

    change style

--- src/component/list.js
+++ src/component/list.js
@@ -3,17 +3,19 @@ import {  CardImg, Button } from 'reactstrap';
 import './list.css'
 
 const List = (props) => {
-     return (
-         
-    <div class="singleCard">
+
+     let colorType = props.type==='Add'? 'primary':'danger';
+
+     return (     
+        <div class="singleCard">
            <CardImg className="movieimg" src={props.movie['img']} top alt="Avatar" />
            <div class="moviebody">
                  <p>{props.movie['title']}</p> 
            </div>
           <div className="footer">
-               <Button className="controlButton" onClick={() => props.manipulate(props.movie['id'])}>{props.type}</Button>
+               <Button color={colorType} className="controlButton" onClick={() => props.manipulate(props.movie['id'])}>{props.type}</Button>
           </div>
-    </div>
+       </div>
 
 
      )
